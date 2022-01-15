9c2fc85fea60d219afea67a8221fca401e8f2c2d
pangpengqiang <pangpengqiang@163.com>
2022-01-15 17:23:30 +0800

    1.15

--- alsa/alsa.c
+++ alsa/alsa.c
@@ -6,9 +6,11 @@
 #include <math.h>
 #include <alsa/asoundlib.h>
 
-#define SAMPLE_RATE (44100)
-#define CHANNELS (2)
-#define FRAME_SIZE (2 * CHANNELS)
+#include "wav.h"
+
+// #define SAMPLE_RATE (16000)
+// #define CHANNELS (2)
+// #define FRAME_SIZE (2 * CHANNELS)
 // snd_pcm_hw_params_alloca snd_pcm_hw_params_set_format
 int audio_play(const char *filename)
 {
@@ -25,10 +27,25 @@ int audio_play(const char *filename)
     int err;
 
     fd = open(filename, O_RDONLY);
-
+    Wav wav_format;
+    printf("WAV_fotmat size:%ld\n", sizeof(Wav));
+    rc = read(fd, &wav_format, sizeof(Wav));
+    if (rc != sizeof(Wav))
+    {
+        fprintf(stderr, "read wav erroe\n");
+        exit(1);
+    }
+    printf("ChunkSize:%d\n", wav_format.riff.ChunkSize);
+    printf("AudioFormat:%d\n", wav_format.fmt.AudioFormat);
+    printf("NumChannels:%d\n", wav_format.fmt.NumChannels);
+    printf("SampleRate:%d\n", wav_format.fmt.SampleRate);
+    printf("ByteRate:%d\n", wav_format.fmt.ByteRate);
+    printf("BlockAlign:%d\n", wav_format.fmt.BlockAlign);
+    printf("BitsPerSample:%d\n", wav_format.fmt.BitsPerSample);
+    printf("Subchunk2Size:%d\n", wav_format.data.Subchunk2Size);
     snd_pcm_t *handle;
     //以播放模式打开设备
-    rc = snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
+    rc = snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, SND_PCM_ASYNC); //SND_PCM_ASYNC
     if (rc < 0)
     {
         fprintf(stderr, "unable to open pcm device: %s\n", snd_strerror(rc));
@@ -37,9 +54,10 @@ int audio_play(const char *filename)
     //配置硬件参数结构体
     snd_pcm_hw_params_t *params;
 
+#if 0
     //params申请内存
     snd_pcm_hw_params_malloc(&params);
-
+    
     //使用pcm设备初始化hwparams
     err = snd_pcm_hw_params_any(handle, params);
     if (err < 0)
@@ -58,8 +76,8 @@ int audio_play(const char *filename)
     }
 
     //设置16位采样格式，S16为有符号16位,LE是小端模式
-    snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE
-        err = snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE);
+    snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
+    err = snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE);
     if (err < 0)
     {
         fprintf(stderr, "Failed to set PCM device to 16-bit signed PCM: %s\n", snd_strerror(err));
@@ -67,7 +85,7 @@ int audio_play(const char *filename)
     }
 
     //设置声道数,双声道
-    err = snd_pcm_hw_params_set_channels(handle, params, CHANNELS);
+    err = snd_pcm_hw_params_set_channels(handle, params, wav_format.fmt.NumChannels);
     if (err < 0)
     {
         fprintf(stderr, "Failed to set PCM device to mono: %s\n", snd_strerror(err));
@@ -75,7 +93,7 @@ int audio_play(const char *filename)
     }
 
     //采样率44.1KHZ
-    val = SAMPLE_RATE;
+    val = wav_format.fmt.SampleRate;
     //设置采样率,如果采样率不支持，会用硬件支持最接近的采样率
     err = snd_pcm_hw_params_set_rate_near(handle, params, &val, &dir);
     if (err < 0)
@@ -106,8 +124,7 @@ int audio_play(const char *filename)
         fprintf(stderr, "Failed to set PCM device to period time =%d: %s\n", period_time, snd_strerror(err));
         exit(1);
     }
-
-    snd_pcm_uframes_t buffer_size;
+        snd_pcm_uframes_t buffer_size;
     snd_pcm_hw_params_get_buffer_size_max(params, &buffer_size);
     snd_pcm_hw_params_set_buffer_size_near(handle, params, &buffer_size);
 
@@ -121,15 +138,52 @@ int audio_play(const char *filename)
         fprintf(stderr, "unable to set hw parameters: %s\n", snd_strerror(rc));
         exit(1);
     }
+#else
+    if ((err = snd_pcm_set_params(handle,
+                                  SND_PCM_FORMAT_S16,
+                                  SND_PCM_ACCESS_RW_INTERLEAVED,
+                                  wav_format.fmt.NumChannels,
+                                  wav_format.fmt.SampleRate,
+                                  0,
+                                  4 * 1000000)) < 0)
+    {
+        printf("Playback open error: %s\n", snd_strerror(err));
+        exit(1);
+    }
 
-    int bit = snd_pcm_format_physical_width(format);
+    snd_pcm_hw_params_alloca(&params);
+    /* get the current hwparams */
+    err = snd_pcm_hw_params_current(handle, params);
+    if (err < 0)
+    {
+        printf("Unable to determine current params: %s\n", snd_strerror(err));
+        exit(1);
+    }
+
+    if (snd_pcm_hw_params_supports_audio_ts_type(params, 0))
+        printf("Playback supports audio compat timestamps\n");
+    if (snd_pcm_hw_params_supports_audio_ts_type(params, 1))
+        printf("Playback supports audio default timestamps\n");
+    if (snd_pcm_hw_params_supports_audio_ts_type(params, 2))
+        printf("Playback supports audio link timestamps\n");
+    if (snd_pcm_hw_params_supports_audio_ts_type(params, 3))
+        printf("Playback supports audio link absolute timestamps\n");
+    if (snd_pcm_hw_params_supports_audio_ts_type(params, 4))
+        printf("Playback supports audio link estimated timestamps\n");
+    if (snd_pcm_hw_params_supports_audio_ts_type(params, 5))
+        printf("Playback supports audio link synchronized timestamps\n");
+#endif
+
+    // int bit = snd_pcm_format_physical_width(format);
     snd_pcm_hw_params_get_period_size(params, &frames, &dir);
     // 1 frame = channels * sample_size.
-    size = frames * FRAME_SIZE; /* 2 bytes/sample, 1 channels */
+    int frames_size = wav_format.fmt.NumChannels * (wav_format.fmt.BitsPerSample / 8);
+    size = frames * frames_size; /* 2 bytes/sample, 1 channels */
     buffer = (char *)malloc(size);
 
     while (1)
     {
+        // usleep(50*1000);
         rc = read(fd, buffer, size);
         if (rc == 0)
         {
@@ -140,7 +194,7 @@ int audio_play(const char *filename)
         {
             fprintf(stderr, "short read: read %d bytes\n", rc);
         }
-        rc = snd_pcm_writei(handle, buffer, rc / FRAME_SIZE);
+        rc = snd_pcm_writei(handle, buffer, rc / frames_size); //rc / FRAME_SIZE
         if (rc == -EPIPE)
         {
             fprintf(stderr, "underrun occurred\n");
--- alsa/main.c
+++ alsa/main.c
@@ -4,8 +4,10 @@
 int main(int agrc, char *agrv[])
 {
     printf("main start \n");
-
-    audio_play("test.pcm");
+    if (agrc <= 1)
+        return -1;
+    printf("audio_play:%s\n",agrv[1]);
+    audio_play(agrv[1]);
 
     printf("main end\n");
     return 0;
